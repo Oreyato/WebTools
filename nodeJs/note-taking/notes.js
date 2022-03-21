0dca923a536c10cb61c6444d3d150e397749accf
Oreyato <oreyato.pro@gmail.com>
2022-03-21 17:51:21 +0100

    a notes module #2

---
+++ nodeJs/note-taking/notes.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+function saveNotes(notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', 'dataJSON');
+}
+
+function loadNotes() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+
+        return JSON.parse(dataJSON);
+    } catch(e) { // if the first part didn't work ~if notes.json doesn't exist or is badly formatted for example
+        return [];
+    }
+};
+
+function addNote(title, body) {
+    const notes = loadNotes();
+    notes.push({title: title, body: body});
+    saveNotes(notes);
+};
+
+
