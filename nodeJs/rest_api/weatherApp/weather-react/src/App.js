5a231160f17fee14006e467f09a7ba2791be2e6a
Oreyato <oreyato.pro@gmail.com>
2022-03-23 17:10:23 +0100

    a react app

--- nodeJs/rest_api/weatherApp/weather-react/src/App.js
+++ nodeJs/rest_api/weatherApp/weather-react/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Body from './Body.js';
+import Header from './Header.js';
+import Menu from './Menu.js';
+import Footer from './Footer.js';
 
 // More infos here: https://reactjs.org/
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <Header/>
+      <Menu/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
---
+++ nodeJs/rest_api/weatherApp/weather-react/src/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <p><a href="RIRI">RIRI</a> | <a href="FIFI">FIFI</a> | <a href="LOULOU">LOULOU</a></p>
+        );
+    }
+}
+
+export default Menu; 
