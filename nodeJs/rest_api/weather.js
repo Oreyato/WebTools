786235c9627c654f0cf00bd8527eeedeb71032b5
Oreyato <oreyato.pro@gmail.com>
2022-03-23 14:55:07 +0100

    first weather app

--- nodeJs/rest_api/weather.js
+++ nodeJs/rest_api/weather.js
@@ -1,3 +1,5 @@
+// Weather with mapbox + darksky
+
 const request = require('request');
 
 const mapboxToken = 'pk.eyJ1IjoiZ2FldHoiLCJhIjoiY2s1cDdlcG8xMHQyYjNmbnN0YjJhcmNqeiJ9.kLu5p2ln7vq0X7CyQARZfQ';
@@ -43,22 +45,64 @@ function functionName(data, callback) {
 };
 */
 
-function geocode(city, callback) {
-    url = geocodeUrl;
+function cityLocation(city, callback) {
+    // -Les URI sont la technologie de base du World Wide Web car tous les hyperliens du Web sont exprimés sous forme d'URI-
+    const encodedCity = encodeURIComponent(city);
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodedCity + '.json?&language=fr&access_token=' + mapboxToken;
 
     request({url: url, json: true}, (error, response) => {
         if (error) {
-            callback("Server not reached error");
+            callback("Unable to connect to geocode API");
         } else if (response.body.error) { 
-            callback("Server error"); 
+            callback("Unable to find location"); 
         } else {
             latitude = response.body.features[0].center[1];
             longitude = response.body.features[0].center[0];
     
+            const data = {latitude, longitude}; // Shortcut for { latitude: latitude, longitude: longitude }
+
+            console.log("Météo à " + city + ":");
+            /*
             console.log(city + "'s location:");
             console.log("\tLongitude: " + longitude + "\n\tLatitude: " + latitude);
-            
-            callback(error, newData);
+            */
+
+            callback(error, data);
         }
     });
 }
+
+function displayWeather(data, callback) {
+    const url = 'https://api.darksky.net/forecast/ad95e11dbf8fe6961a05425dea338c24/' + data.latitude + "," + data.longitude + '?units=si&lang=fr';
+
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback("Unable to connect to weather API");
+        } else if (response.body.error) {
+            callback("Unable to find location\'s weather");
+        } else {
+            // Destructuring to create the two variables
+            const {temperature, precipProbability} = response.body.currently; 
+            /// It is equivalent to
+            /// const temperature = response.body.currently.temperature;
+            /// const precipProbability = response.body.currently.precipProbability
+
+            callback(undefined, response.body.daily.data[0].summary + " Il fait actuellement " + temperature + '°C et il y a ' + precipProbability + "% de chance qu'il pleuve");
+        }
+    });
+}
+
+// Chaining functions
+function weather(city, callback) {
+    cityLocation(city, (error, data) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        displayWeather(data, callback);
+    });
+}
+
+weather('Londres', (error, data) => {
+    console.log(data);
+});
