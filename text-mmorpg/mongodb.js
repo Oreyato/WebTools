31634cde8c3cd70818f4299ddb08727395c881fb
Oreyato <oreyato.pro@gmail.com>
2022-11-24 18:08:59 +0100

    241122 - lesson

---
+++ text-mmorpg/mongodb.js
@@ -0,0 +1,32 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017'; // Database address
+const databaseName = 'text-mmorpg';
+
+MongoClient.connect(connectionURL, { useNewURLParser:true }, (error, client) => {
+    if(error) {
+        return console.log('Unable to connect to database')
+    }
+    console.log('Connected to database')
+    const db = client.db(databaseName)
+    // db.collection('players').insertMany([
+    //     {
+    //         name: 'Gaëtan',
+    //         level: 34,
+    //         acceptPvp: false
+    //     }, {
+    //         name: 'Tan',
+    //         level: 5,
+    //         acceptPvp: true
+    //     }, {
+    //         name: 'Angèle',
+    //         level: 23,
+    //         acceptPvp: true
+    //     }
+    // ]);
+
+    db.collection('players').find({acceptPvp: true}).count((error, count) => {
+        console.log(count)
+    })
+});
